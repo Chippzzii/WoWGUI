6915698ed2fe6ede16f4df9caaaec1b0a2f04987
c.hülsmann <c.huelsmann93@gmail.com>
2023-03-05 18:34:39 +0100

    moreUpdates

--- WowGUI/src/main/java/de/wow/wowgui/RunderButton.java
+++ WowGUI/src/main/java/de/wow/wowgui/RunderButton.java
@@ -1,16 +1,17 @@
 package de.wow.wowgui;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class RunderButton extends JButton {
 
     public RunderButton(String label) {
-        super(label);
-        setBackground(Color.GREEN); // Hintergrundfarbe ändern
-        setOpaque(false); // Hintergrund unsichtbar machen
-        setPreferredSize(new Dimension(100, 100)); // Größe setzen
+        setOpaque(false);
+        setContentAreaFilled(false);
     }
 
     @Override
@@ -21,15 +22,23 @@ public class RunderButton extends JButton {
         } else {
             g.setColor(getBackground());
         }
-        g.fillOval(0, 0, getSize().width - 1, getSize().height - 1);
+//        g.fillOval(0, 0, getSize().width, getSize().height);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         super.paintComponent(g);
 
     }
 
+
+
     @Override
     protected void paintBorder(Graphics g) {
-        g.setColor(getForeground());
-        g.drawOval(0, 0, getSize().width - 1, getSize().height - 1);
+        setOpaque(false);
+        setBorderPainted(false);
+        g.setColor(Color.gray);
+        g.drawOval(0, 0, getSize().width-1, getSize().height-1);
     }
 
 
--- WowGUI/src/main/java/de/wow/wowgui/Wahrsagekugel.java
+++ WowGUI/src/main/java/de/wow/wowgui/Wahrsagekugel.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,7 +29,6 @@ public class Wahrsagekugel extends JFrame {
     public Wahrsagekugel() {
         super("Wahrsagekugel");
         ImageIcon im = new ImageIcon("hintergrundTest.png");
-
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -38,6 +39,7 @@ public class Wahrsagekugel extends JFrame {
                 try {
                     Image background = ImageIO.read(new File("C:\\GitHub\\WoWGUI\\WowGUI\\src\\main\\java\\de\\wow\\wowgui\\hintergrundTest.png"));
                     g2d.drawImage(background, 0, 0, null);
+
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
@@ -46,13 +48,33 @@ public class Wahrsagekugel extends JFrame {
             }
         };
 
-        panel.setBounds(250, 250, im.getIconWidth(), im.getIconHeight());
+        panel.setBounds(0, 0, im.getIconWidth(), im.getIconHeight());
         panel.setLayout(null);
 
-        button = new RunderButton("Drück mich!!");
-        button.setBounds(250, 275, 110, 70);
-        button.setForeground(Color.BLACK);
-        button.setBackground(Color.BLUE);
+
+        button = new RunderButton("Drück mich!!!");
+
+
+        try{
+            button.setBounds(250, 275, 110, 70);
+            Image ima = ImageIO.read(new File("C:\\GitHub\\WoWGUI\\WowGUI\\src\\main\\java\\de\\wow\\wowgui\\buttontest.png"));
+
+            Shape shape = new Ellipse2D.Float(0,0, button.getWidth(), button.getHeight());
+            BufferedImage mI = new BufferedImage(button.getWidth(), button.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = mI.createGraphics();
+            g2d.setClip(shape);
+            g2d.drawImage(ima, 0,0,null);
+            g2d.dispose();
+
+            Icon bgIcon = new ImageIcon(mI);
+            button.setIcon(bgIcon);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -64,7 +86,6 @@ public class Wahrsagekugel extends JFrame {
 
         ausgabe = new JLabel();
         ausgabe.setBounds(80, 30, 250, 30);
-        ausgabe.setText("Hallo Welt");
         ausgabe.setForeground(Color.WHITE);
         ausgabe.setBackground(Color.white);
         ausgabe.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -79,9 +100,6 @@ public class Wahrsagekugel extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-
-
-
 
     }
 
@@ -99,7 +117,8 @@ public class Wahrsagekugel extends JFrame {
         variations.add("Kräuter farmen");
         variations.add("Leder farmen");
         variations.add("Dungeons grinden");
-        variations.add("In einer Hauptstadt deiner Wahl chillen");
+        variations.add("In einer Hauptstadt deiner Wahl chillen.");
+        variations.add("Ruf einer Fraktion deiner Wahl farmen.");
 
         zufall = rnd.nextInt(variations.size());
 
